e89b945d4cec22ab3928c6487339387ae304f9c5
Janne Pulkkinen <janne.pulkkinen@protonmail.com>
2021-04-04 14:59:23 +0300

    Run CI on Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -259,6 +259,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
